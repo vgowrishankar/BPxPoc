23b59d6f4ed9c4ee65e0e86afabffb5ab6cec2b7
Gowrishankar Venkatesan <gowrishankar.10656162@ltimindtree.com>
2023-12-07 13:45:31 +0530

    update page updated for login

--- Resources/PageObject/Login_Page.py
+++ Resources/PageObject/Login_Page.py
@@ -1,3 +1,3 @@
-Login_page_username ="id=email"
-Login_page_password="id=pass"
-Login_page_login_btn="name=login"
+Login_page_username = "id=email"
+Login_page_password = "id=pass"
+Login_page_login_btn = "name=login"
--- Tests/Page_Object/Login_Page.py
+++
@@ -1,3 +0,0 @@
-Login_page_username = "id=email"
-Login_page_password = "id=pass"
-Login_page_login_btn = "name=login"
